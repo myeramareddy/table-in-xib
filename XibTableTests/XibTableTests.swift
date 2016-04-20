b0dc7fb74c9f37893cf539928a4599533bcb7f0b
Manisha Yeramareddy <manisha.yeramareddy@cashcall.com>
2016-04-19 19:37:07 -0700

    Initial Commit

---
+++ XibTableTests/XibTableTests.swift
@@ -0,0 +1,36 @@
+//
+//  XibTableTests.swift
+//  XibTableTests
+//
+//  Created by Manisha Yeramareddy on 4/19/16.
+//  Copyright © 2016 Manisha Yeramareddy. All rights reserved.
+//
+
+import XCTest
+@testable import XibTable
+
+class XibTableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
